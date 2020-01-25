c5fd4ad9f78230f3be1b26e4bc5bfeeb18a22fa1
Denis Pozdnyakov <oooeeee@gmail.com>
2020-01-25 22:59:28 +0300

    Added some dummy back and storage class

---
+++ electricity-backend/app.py
@@ -0,0 +1,90 @@
+import os
+import html
+import logging
+from modules.Storage import Storage
+from flask import Flask, request, jsonify
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.WARNING)
+
+
+def run_app():
+    static_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), 'frontend', 'dist'))
+    app = Flask(__name__, static_folder=static_folder, static_url_path='/static')
+    storage = Storage()
+
+    @app.route("/")
+    def root():
+        return app.send_static_file("index.html")
+
+    @app.route("/storage/streets", methods=["GET"])
+    def get_street_names():
+        return jsonify(storage.get_street_names())
+
+    @app.route("/storage/street/<street_name>/latest", methods=["GET"])
+    def get_street_latest_data(street_name):
+        raise NotImplementedError()
+
+    @app.route("/storage/add_latest_date", methods=["POST"])
+    def add_today():
+        raise NotImplementedError()
+
+    @app.route("/storage/street/<street_name>/house/<house>/set/<data_type>/<data_value>", methods=["PUT"])
+    def put_latest_house_data(street_name, house, data_type, data_value):
+        raise NotImplementedError()
+
+    @app.errorhandler(500)
+    def internal_error(error):
+        err_template = '<h1>An error has occurred</h1><br/> Some additional info:<br/><pre>%s</pre>'
+        err_escaped = html.escape(str(error))
+        response_html = err_template % err_escaped
+        return response_html, 500
+
+    return app
+
+
+app = run_app()
+app.wsgi_app = ProxyFix(app.wsgi_app)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 600
+if __name__ == '__main__':
+    @app.before_request
+    def option_autoreply():
+        """ Always reply 200 on OPTIONS request """
+
+        if request.method == 'OPTIONS':
+            resp = app.make_default_options_response()
+
+            headers = None
+            if 'ACCESS_CONTROL_REQUEST_HEADERS' in request.headers:
+                headers = request.headers['ACCESS_CONTROL_REQUEST_HEADERS']
+
+            h = resp.headers
+
+            # Allow the origin which made the XHR
+            h['Access-Control-Allow-Origin'] = request.headers['Origin']
+            # Allow the actual method
+            h['Access-Control-Allow-Methods'] = request.headers['Access-Control-Request-Method']
+            # Allow for 10 seconds
+            h['Access-Control-Max-Age'] = "10"
+
+            # We also keep current headers
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+
+            return resp
+
+    @app.after_request
+    def set_allow_origin(resp):
+        """ Set origin for GET, POST, PUT, DELETE requests """
+
+        h = resp.headers
+
+        # Allow crossdomain for other HTTP Verbs
+        if request.method != 'OPTIONS' and 'Origin' in request.headers:
+            h['Access-Control-Allow-Origin'] = request.headers['Origin']
+
+        return resp
+
+    app.run(host="0.0.0.0", port=8080, threaded=True)
---
+++ electricity-backend/modules/Storage.py
@@ -0,0 +1,38 @@
+import os
+import json
+import enum
+
+
+class AllowedDataTypes(enum.Enum):
+    DAY = 'DAY'
+    NIGHT = 'NIGHT'
+
+
+def get_storage_filename():
+    dir_name = os.path.dirname(__file__)
+    real_data_file = os.path.join(dir_name, 'data.json')
+    demo_data_file = os.path.join(dir_name, 'demo_data.json')
+    return real_data_file if os.path.exists(real_data_file) else demo_data_file
+
+
+class Storage:
+    def __init__(self):
+        filename = get_storage_filename()
+        with open(filename, 'r') as file:
+            self._data = json.load(file)
+
+    def get_street_names(self):
+        return list(self._data.keys())
+
+    def get_street(self, street_name):
+        assert street_name in self._data
+        return self._data[street_name]
+
+    def get_house(self, street_name, house):
+        street = self.get_street(street_name)
+        assert house in street_name
+        return street[house]
+
+    def update_data(self, street_name, house, data_type, data):
+        house = self.get_house(street_name, house)
+        raise NotImplementedError()
