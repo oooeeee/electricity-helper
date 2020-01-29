e56f18e3090e79c23f641fa6653ab712b2854f9d
Denis Pozdnyakov <oooeeee@gmail.com>
2020-01-29 09:00:10 +0300

    Added some events to house, and button "back" to street

--- frontend/src/components/House.vue
+++ frontend/src/components/House.vue
@@ -1,28 +1,32 @@
 <template>
-  <div>
-    <div v-if="!edit_mode">
-      <button type="button" class="btn btn-primary" @click.prevent="edit_mode = !edit_mode">{{this.house}}</button>
-    </div>
-    <div v-else class="table-row" v-for="(date_info, date_index) in house_info" :key="date_index">
-      <div class="table-cell tc-10">
-        <div class="labels">
-          <span class="badge badge-info">{{ date_info.date }}</span>
+  <div v-if="show_house">
+    <div v-if="edit_mode">
+      <center><h3>House {{house}}</h3></center>
+      <div class="table-row" v-for="(date_info, date_index) in house_info" :key="date_index">
+        <div class="table-cell tc-10">
+          <div class="labels">
+            <span class="badge badge-info">{{ date_info.date }}</span>
+          </div>
+        </div>
+        <div class="table-cell tc-30" v-for="(data_value, data_type) in date_info" :key="data_type">
+          <UpdateButton 
+            :street_name="street_name"
+            :house_name="house"
+            :date="date_info.date"
+            :data_type="data_type"
+            :data_value="data_value"
+          />
         </div>
       </div>
-      <div class="table-cell tc-30" v-for="(data_value, data_type) in date_info" :key="data_type">
-        <UpdateButton 
-          :street_name="street_name"
-          :house_name="house"
-          :date="date"
-          :data_type="data_type"
-          :data_value="data_value"
-        />
-      </div>
+    </div>
+    <div v-else>
+      <button type="button" class="btn btn-primary" @click.prevent="switchToEditMode()">{{this.house}}</button>
     </div>
   </div>
 </template>
 <script>
 
+import Bus from '../bus'
 import { common_state_store } from '../shared'
 import UpdateButton from './UpdateButton.vue'
 
@@ -32,14 +36,30 @@ export default {
   },
   props: {
     street_name: { type: String, required: true },
-    house_info: { type: Object, required: true },
+    house_info: { type: Array, required: true },
     house: {type: String, required: true },
+  },
+
+  created() {
+    Bus.$on('hide_houses', () => {if (!this.edit_mode) this.show_house = false})
+    Bus.$on('show_houses', () => {this.show_house = true; this.edit_mode = false})
   },
 
   data: () => ({
     common_store: common_state_store,
     edit_mode: false,
+    show_house: true,
   }),
 
+  methods: {
+    switchToEditMode() {
+      this.edit_mode = true;
+      Bus.$emit("hide_houses")
+    },
+    returnToCommonMode() {
+      Bus.$emit("show_houses")
+    }
+  },
 }
+
 </script>
--- frontend/src/components/Streets.vue
+++ frontend/src/components/Streets.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {{active_street?active_street:'Choose street'}}
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a v-for="(street, street_index) in streets" :key="street_index" class="dropdown-item" href="#" @click.prevent="requestStreet(street)"> {{street}}</a>
+    <div class="container">
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {{active_street?active_street:'Choose street'}}
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a v-for="(street, street_index) in streets" :key="street_index" class="dropdown-item" href="#" @click.prevent="requestStreet(street)"> {{street}}</a>
+        </div>
       </div>
+      <button v-if="show_back_button" type="button" class="btn btn-primary" @click.prevent="back_houses()">Back</button>
     </div>
-    <div v-if="this.active_street" class="houses">
+    <div v-if="this.active_street" class="container">
       <div v-for="(house_info, house_index) in street_info" :key="house_index">
         <House :house=house_index :house_info=house_info :street_name=active_street />
       </div>
@@ -29,6 +32,7 @@ export default {
   data: () => ({
     common_store: common_state_store,
     active_street: '',
+    show_back_button: false,
   }),
 
   computed: {
@@ -39,8 +43,15 @@ export default {
       return this.common_store.state.street
     },
   },
+  created() {
+    Bus.$on('show_houses', () => {this.show_back_button = false})
+    Bus.$on('hide_houses', () => {this.show_back_button = true})
+  },
 
   methods: {
+    back_houses() {
+      Bus.$emit('show_houses')
+    },
     requestStreet(street) {
       this.active_street = street;
       Bus.$emit('update_street', this.active_street, true);
@@ -50,7 +61,7 @@ export default {
 </script>
 
 <style>
-.houses {
+.container {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
