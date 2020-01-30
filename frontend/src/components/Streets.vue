9960cbcab7ec1eafff55f380f39ac7cc1e39e654
Denis Pozdnyakov <oooeeee@gmail.com>
2020-01-30 14:53:50 +0300

    Added modal window to add latest date

--- frontend/src/components/Streets.vue
+++ frontend/src/components/Streets.vue
@@ -10,11 +10,33 @@
           <a class="dropdown-item" :href="base_url + '/storage/export.xlsx'">Export all</a>
         </div>
       </div>
-      <button v-if="show_back_button" type="button" class="btn btn-primary" @click.prevent="back_houses()">Back</button>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#createTodayModal">Create 'today' date</button>
+      <button v-bind:class="{'disabled': !show_back_button}" type="button" class="btn btn-primary" @click.prevent="back_houses()">Back</button>
     </div>
     <div v-if="this.active_street" v-bind:class="{'streets-container': !show_back_button}">
       <div v-for="(house_info, house_index) in street_info" :key="house_index">
         <House :house=house_info.house :house_info=house_info.date_info :street_name=active_street />
+      </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="createTodayModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            You want to add "today" date to all houses.
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Nope</button>
+            <AsyncButton :url="'/storage/add_latest_date'" method='post' text="Yes" data-dismiss='modal'/>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -23,10 +45,12 @@
 <script>
 import Bus from '../bus'
 import House from './House.vue'
+import AsyncButton from './AsyncButton.vue'
 import { BASE_URL, common_state_store } from '../shared'
 
 export default {
   components: {
+    AsyncButton,
     House
   },
 
